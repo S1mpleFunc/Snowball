46b8d730da1f6aa80449a986cbcacb6d901b1b5c
S1mpleFunc <42806772+S1mpleFunc@users.noreply.github.com>
2019-08-20 17:29:12 +0300

    теперь на пикабу

--- src/main/java/ru/func/snowball/Snowball.java
+++ src/main/java/ru/func/snowball/Snowball.java
@@ -17,14 +17,19 @@ public class Snowball {
         /* Поиск фото на посте */
         Document doc = Jsoup.connect(args[0]).get();
         String urlUpload = "";
-        for (Element element : doc.getElementsByClass("image").get(0).children())
-            urlUpload = element.getElementsByTag("img").attr("src");
+        for (Element e : doc.getElementsByTag("img")) {
+            if (e.hasAttr("data-large-image")) {
+                System.out.println(e.attr("data-src"));
+                if (!urlUpload.isEmpty())
+                    break;
+            }
+        }
 
         /* Скачивание фото */
         URL url = new URL(urlUpload);
-        File downloadDir = new File(args[1] + "/" + (new Random().nextInt(9000) + 1000) + ".jpeg");
+        File downloadDir = new File(args[1] + "/" + (new Random().nextInt(9000) + 1000) + ".jpg");
         RenderedImage img = ImageIO.read(url);
-        ImageIO.write(img, "jpeg", downloadDir);
+        ImageIO.write(img, "jpg", downloadDir);
 
         System.out.println("Файл успешно скачан.");
     }
